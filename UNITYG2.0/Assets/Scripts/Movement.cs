1ae2414f0a7a7f8dd5c3b905c7c0abb816aa3e53
Hi In This <qrtqrt50@outlook.com>
2022-08-16 21:27:56 +0300

    ...

--- UNITYG2.0/Assets/Scripts/Movement.cs
+++ UNITYG2.0/Assets/Scripts/Movement.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.InputSystem;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int J_Force;
+    private bool IS_J;
+    private Rigidbody2D Rigidbody_2D;
+
+    private void Start()
     {
-        
+        IS_J = false;
     }
-    
-    // Update is called once per frame
-    void Update()
+
+    private void Awake()
     {
-        
+        Rigidbody_2D = GetComponent<Rigidbody2D>();
+    }
+
+    public void jump(InputAction.CallbackContext CON)
+    {
+        if (IS_J == true)
+        {
+            Debug.Log($"JUMP! {CON.phase}");
+            Rigidbody_2D.AddForce(Vector3.up * J_Force, ForceMode2D.Impulse);
+        }
     }
 }
